391d335c0785a7bb757911eca958f7d482971242
Argonui <argonui13@gmail.com>
2022-11-03 14:04:44 -0500

    Allow LuaReader to read from multiple directories

--- file/luaops.go
+++ file/luaops.go
@@ -13,8 +13,10 @@ const (
 
 // LuaOps allows for arbitrary reads and writes of luascript
 type LuaOps struct {
-	basepath        string
-	readFileToBytes func(string) ([]byte, error)
+	basepaths        []string
+	writeBasepath    string
+	readFileToBytes  func(string) ([]byte, error)
+	writeBytesToFile func(string, []byte) error
 }
 
 // LuaReader serves to describe all ways to read luascripts
@@ -29,8 +31,14 @@ type LuaWriter interface {
 
 // NewLuaOps initializes our object on a directory
 func NewLuaOps(base string) *LuaOps {
+	return NewLuaOpsMulti([]string{base}, base)
+}
+
+// NewLuaOpsMulti allows for luascript to be read from multiple directories
+func NewLuaOpsMulti(readDirs []string, writeDir string) *LuaOps {
 	return &LuaOps{
-		basepath: base,
+		basepaths:     readDirs,
+		writeBasepath: writeDir,
 		readFileToBytes: func(s string) ([]byte, error) {
 			sFile, err := os.Open(s)
 			if err != nil {
@@ -40,23 +48,28 @@ func NewLuaOps(base string) *LuaOps {
 
 			return ioutil.ReadAll(sFile)
 		},
+		writeBytesToFile: func(p string, b []byte) error {
+			return os.WriteFile(p, b, 0644)
+		},
 	}
 }
 
 // EncodeFromFile pulls a file from configs and encodes it as a string.
 func (l *LuaOps) EncodeFromFile(filename string) (string, error) {
-	p := path.Join(l.basepath, filename)
-	b, err := l.readFileToBytes(p)
-	if err != nil {
-		return "", err
+	for _, base := range l.basepaths {
+		p := path.Join(base, filename)
+		b, err := l.readFileToBytes(p)
+		if err != nil {
+			continue
+		}
+		s := string(b)
+		return s, nil
 	}
-	s := string(b)
-
-	return s, nil
+	return "", fmt.Errorf("%s not found among any known paths", filename)
 }
 
 // EncodeToFile takes a single string and decodes escape characters; writes it.
 func (l *LuaOps) EncodeToFile(script, file string) error {
-	p := path.Join(l.basepath, file)
-	return os.WriteFile(p, []byte(script), 0644)
+	p := path.Join(l.writeBasepath, file)
+	return l.writeBytesToFile(p, []byte(script))
 }
