7ad1194554cc79544da6cfa434a4d02914707702
Buhallin <keallen.42@gmail.com>
2022-11-08 13:52:05 -0800

    Correct test values for numbersmoother

--- objects/numbersmoother_test.go
+++ objects/numbersmoother_test.go
@@ -8,15 +8,15 @@ import (
 
 func TestPosition(t *testing.T) {
 	j := map[string]interface{}{
-		"posX": 42.41,
-		"posY": 1.5,
-		"posZ": -11.36,
+		"posX": 42.4101944,
+		"posY": 1.49994421,
+		"posZ": -11.3552332,
 	}
 	got := Smooth(j)
 	want := map[string]interface{}{
-		"posX": 42.4102,
-		"posY": 1.4999,
-		"posZ": -11.3552,
+		"posX": 42.41,
+		"posY": 1.5,
+		"posZ": -11.36,
 	}
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("want != got:\n%v\n", diff)
